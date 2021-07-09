a5036fe47fe4b8d8a55dc8fa3bda2765e35447b7
minsangchoi <87048430+minsangchoi@users.noreply.github.com>
2021-07-09 09:35:30 +0900

    Update MemberController.java

--- src/main/java/com/shop/controller/MemberController.java
+++ src/main/java/com/shop/controller/MemberController.java
@@ -114,9 +114,9 @@ public class MemberController {
         /* 이메일 보내기 */
         String setFrom = "1111@naver.com";
         String toMail = email;
-        String title = "재현 쇼핑몰 회원가입 인증 이메일 입니다.";
+        String title = "이젠 쇼핑몰 회원가입 인증 이메일 입니다.";
         String content = 
-                "재현 쇼핑몰을 방문해주셔서 감사합니다." +
+                "이젠 쇼핑몰을 방문해주셔서 감사합니다." +
                 "<br><br>" + 
                 "인증 번호는 " + checkNum + "입니다." + 
                 "<br>" + 
